49b1995862a5acd17ef42f3f2c8de503e02637fe
MoezKouni <mo.elkouni@gmail.com>
2018-05-10 18:27:27 +0100

    To Do Note

---
+++ src/config/config.js
@@ -0,0 +1,8 @@
+export const DB_CONFIG = {
+    apiKey: "AIzaSyCWG_vbZDLCvnj9Ye9PE0F5B1LOz-pNVWk",
+    authDomain: "note-app-e2131.firebaseapp.com",
+    databaseURL: "https://note-app-e2131.firebaseio.com",
+    projectId: "note-app-e2131",
+    storageBucket: "note-app-e2131.appspot.com",
+    messagingSenderId: "41757005197"
+  };
